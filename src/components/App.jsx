f42564c258989f7d34f71f3c7986f1e099d457d2
Anderson Carubelli <acarubelli@gmail.com>
2018-01-12 09:57:46 -0200

    listing reminders

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React, {Component } from 'react';
 import { connect } from 'react-redux';
-import { addReminder } from '../actions';
+import { addReminder, deleteReminder } from '../actions';
 
 class App extends Component {
   constructor(props){
@@ -14,8 +14,32 @@ class App extends Component {
     this.props.addReminder(this.state.text);
   }
 
+  deleteReminder(id){
+    console.log('deleting in application', id);
+    console.log('this.props', this.props);
+  }
+
   renderReminders(){
     const { reminders } = this.props;
+    return(
+      <ul className="list-group col-sm-4">
+      {
+        reminders.map(reminder => {
+          return(
+            <li key={reminder.id} className="list-group-item">
+              <div className="list-item">{reminder.text}</div>
+              <div
+                className="list-item delete-button"
+                onClick={() => this.deleteReminder(reminder.id)}
+              >
+                &#x2715;
+              </div>
+            </li>
+          )
+        })
+      }
+      </ul>
+    )
   }
 
   render(){
@@ -24,14 +48,13 @@ class App extends Component {
         <div className="title">
           Reminder Pro
         </div>
-        <div className="form-inline">
+        <div className="form-inline reminder-form">
           <div className="form-group">
             <input
               className="form-control"
               placeholder="I have to..."
               onChange={event => this.setState({text: event.target.value})}
             />
-            { this.renderReminders() }
           </div>
           <button
             type="button"
@@ -41,6 +64,7 @@ class App extends Component {
             Add Reminder
           </button>
         </div>
+        { this.renderReminders() }
       </div>
     )
   }
@@ -53,4 +77,4 @@ function mapStateToProps(state){
 }
 
 
-export default connect(mapStateToProps, {addReminder})(App);
+export default connect(mapStateToProps, {addReminder, deleteReminder})(App);
